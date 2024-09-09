65a4fdc7e38dbe4b7fc9981c25ba7c3211aca9a9
Adelina Vishnivetskaya <vi.lini@yandex.ru>
2024-09-09 21:53:08 +0300

    done

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+const createCard = (cardData, deleteCallback) => {
+    const cardElement = cardTemplate.firstElementChild.cloneNode(true);
 
-// @todo: Функция создания карточки
+    const image = cardElement.querySelector('.card__image');
+    const title = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+    image.src = cardData.link;
+    image.alt = cardData.name;
+    title.textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+    deleteButton.addEventListener('click', () => {
+        deleteCallback(cardElement);
+    });
+
+    return cardElement;
+}
+
+const deleteCard = (cardElement) => {
+    cardElement.remove();
+}
+
+const renderCards = (cards) => {
+    cards.forEach((cardData) => {
+        const cardElement = createCard(cardData, deleteCard);
+        cardContainer.append(cardElement);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderCards(initialCards);
+});
